92fe891fb988643bf8089ef9c04e278b5f78402b
Matthew Mayers <majormajors@users.noreply.github.com>
2018-09-10 15:35:24 -0700

    Check null

--- src/download.js
+++ src/download.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var links = divs[i].getElementsByTagName("a");
     for (j in links) {
       var matches = links[j].getAttribute("href").match(re);
-      if (matches != undefined) {
+      if (matches != null) {
         links[j].setAttribute("href", "/roms/get-download.php?gid=" + matches[1] + "&test=true");
       }
     }
