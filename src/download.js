e5a7bfe910f8134d95f036aee9360dd90bb0379c
Matthew Mayers <majormajors@users.noreply.github.com>
2018-09-10 14:59:43 -0700

    Add source files and screenshot

---
+++ src/download.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var re = /(\d+)-download$/;
+  var divs = document.getElementsByClassName("download-link");
+  for (i in divs) {
+    var links = divs[i].getElementsByTagName("a");
+    for (j in links) {
+      var matches = links[j].getAttribute("href").match(re);
+      if (matches != undefined) {
+        links[j].setAttribute("href", "/roms/get-download.php?gid=" + matches[1] + "&test=true");
+      }
+    }
+  }
+});
